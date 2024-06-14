1b811f3b99b4fb44e6e2763fc402fbbea75c901f
Deepak <deepakoli.me@gmail.com>
2024-06-14 07:19:28 +0545

    feat: alembic migration

--- app/main.py
+++ app/main.py
@@ -3,24 +3,24 @@ from dotenv import load_dotenv
 
 from app.routers import users, auth, posts, comments, likes, categories
 
-from app.config.database import create_tables
-
+# load environment variables
 load_dotenv()
 
+# create FastAPI instance
 app = FastAPI()
 
+# root route
 @app.get("/")
 def read_root():
     return {"message": "Running"}
 
+#  register routers
 app.include_router(auth.router)
 app.include_router(users.router)
 app.include_router(posts.router)
 app.include_router(categories.router)
 app.include_router(comments.router)
 app.include_router(likes.router)
-
-create_tables()
 
 
 
